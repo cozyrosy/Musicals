aa5236aecaf858208694fcdc215e18458f37c040
cozyrosy <gracejacob.rose@gmail.com>
2022-10-04 15:53:15 +0530

    'addstar'

--- firstEcom/settings.py
+++ firstEcom/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-pz9hm+b#kk5(=bkc=e2w+&bb(g%b16)f_h4sd1gw&)nfv!pzup
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
